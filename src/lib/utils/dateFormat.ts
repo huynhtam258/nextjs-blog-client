0d6beee699bce08e0e8b2b642b9f3a6db1a721dd
HuynhManhTam <huynhtam258@gmail.com>
2024-01-01 22:57:34 +0700

    feat: blog layout

---
+++ src/lib/utils/dateFormat.ts
@@ -0,0 +1,11 @@
+import { formatInTimeZone } from "date-fns-tz";
+
+const dateFormat = (
+  date: Date | string,
+  format: string = "dd MMM, yyyy",
+): string => {
+  // return formatInTimeZone(date, "America/New_York", format);
+  return ''
+};
+
+export default dateFormat;
