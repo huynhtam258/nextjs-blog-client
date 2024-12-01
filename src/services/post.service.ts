7116cce3e28218b576c0a469077b37f5d54c73fb
HuynhManhTam <huynhtam258@gmail.com>
2024-12-01 14:38:47 +0700

    feat: search page

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -4,6 +4,9 @@ export const getPosts = async (params: any) => {
   return await sendRequest<any>({
     url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/post?page=${params.page || 1}&items_per_page=10}`,
     method: "GET",
+    queryParams: {
+      search: params.search
+    }
   }) as any
 }
 
